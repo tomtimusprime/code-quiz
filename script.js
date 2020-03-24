60c8744fa4af17a6958c74a9a319c7d741afbc5c
Tom Black <tom.black06007@gmail.com>
2020-03-23 19:43:30 -0700

    Created the timer and the shell for the javascript quiz.

---
+++ script.js
@@ -0,0 +1,14 @@
+let timerTextElement = document.querySelector("#timer-text");
+let timerCount = 75;
+timerTextElement.textContent = `Time Remaining: ${timerCount}`;
+quizTimer();
+
+function quizTimer() {
+    let timerId = setInterval(function() {
+        timerCount--;
+        timerTextElement.textContent = `Time Remaining: ${timerCount}`;
+        if(timerCount === 0) {
+            clearTimeout(timerId);
+        }
+    }, 1000)
+}
