343565787b069289a8c8934581b362fd6281a28d
Tom Black <tom.black06007@gmail.com>
2020-03-23 21:50:57 -0700

    Added the quiz questions in an array of objects.

--- script.js
+++ script.js
@@ -14,5 +14,52 @@ function quizTimer() {
 }
 
 const questions = [
-    {question}
-]
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    },
+    {
+        question: "Which framework was made by facebook and is in high demand right now?",
+        answers: {
+            a: "Angular",
+            b: "Vue",
+            c: "JQuery",
+            d: "React"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What is the latest version of javscript right now?",
+        answers: {
+            a: "ES5",
+            b: "ES6",
+            c: "ES7",
+            d: "ES4"
+        },
+        correctAnswer: "b",
+    }
+  ];
